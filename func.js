5fbf54d93ce6f243fa989357c33438f48bd8b671
Tomoe-495 <hasnainsiddique495@gmail.com>
2022-08-11 11:34:44 +0500

    added a js file and added about section in the index

---
+++ func.js
@@ -0,0 +1,24 @@
+
+function reveal(){
+    let reveal = document.querySelectorAll('.reveal');
+
+    for(let i = 0; i < reveal.length; i++){
+        let windowheight = window.innerHeight;
+        let revealtop = reveal[i].getBoundingClientRect().top;
+        let revealpoint = 100;
+
+        if(revealtop < windowheight - revealpoint){
+            reveal[i].classList.add('active');
+        }
+    }
+}
+
+
+//      scroll event listener
+window.addEventListener("scroll", () =>{
+    let scrolled = window.scrollY;
+
+    if(scrolled < 200){
+
+    }
+})
