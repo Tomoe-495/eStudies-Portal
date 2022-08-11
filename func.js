82edb8252179a23fac7ec226548932f43e0fb004
Tomoe-495 <hasnainsiddique495@gmail.com>
2022-08-11 12:13:30 +0500

    new feature
    added a transparency feature to the navbar, it'll only stay transparent when it's at the top

--- func.js
+++ func.js
@@ -13,12 +13,15 @@ function reveal(){
     }
 }
 
+let nav = document.getElementsByClassName('nav')[0];
 
 //      scroll event listener
 window.addEventListener("scroll", () =>{
     let scrolled = window.scrollY;
 
-    if(scrolled < 200){
-
+    if(scrolled < 700){
+        nav.style.backgroundColor = "rgba(53, 53, 53, .3)";
+    }else if(scrolled > 700){
+        nav.style.backgroundColor = "rgba(53, 53, 53, 1)"
     }
 })
